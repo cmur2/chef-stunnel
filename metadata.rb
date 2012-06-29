828a86807274b8a55b675eec864ed3135bfeeba7
Jochen Lillich <jochen@lillich.info>
2012-06-29 19:18:42 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jochen@freistil.it"
 license          "All rights reserved"
 description      "Installs/Configures stunnel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 %w( debian ubuntu ).each do |os|
   supports os
